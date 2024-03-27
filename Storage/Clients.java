35d65bf4b5be62c58b1fb4a10a72b983513e9dd9
ElFalsoLicendiado <chopytrollface@gmail.com>
2024-03-26 18:26:17 -0600

    Avance 26/03/24 #7

---
+++ Storage/Clients.java
@@ -0,0 +1,9 @@
+package Storage;
+
+import Objects.Person;
+import Objects.Worker;
+import java.util.ArrayList;
+
+public class Clients {
+    public static ArrayList<Person> clientList = new ArrayList<Person>();
+}
---
+++ Storage/Workers.java
@@ -0,0 +1,8 @@
+package Storage;
+
+import Objects.Worker;
+import java.util.ArrayList;
+
+public class Workers {
+    public static ArrayList<Worker> workerList = new ArrayList<Worker>();
+}
