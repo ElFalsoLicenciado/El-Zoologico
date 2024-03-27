15a689e0cffda312cdeb923051e8234d898e4f15
ElFalsoLicendiado <chopytrollface@gmail.com>
2024-03-27 11:40:28 -0600

    Avance 27/03/24 #1

---
+++ Base/Execution.java
@@ -0,0 +1,49 @@
+package Base;
+
+import java.io.Console;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Execution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Menu zoo = new Menu();
+        Random rn = new Random();
+        final String safePass = "candeChamba";
+        boolean passIsValid = false;
+
+        do {
+            System.out.print("\nIntroduce the password: ");
+            String in = sc.nextLine();
+            System.out.print("\033[H\033[2J");
+
+
+            if (in.equals(safePass)) {
+                passIsValid = true;
+                String n;
+                StringBuilder mes = new StringBuilder("Access granted, please stand by ---------- %00");
+                int x = mes.indexOf("%")+1;
+                for (int i = 1; i<11;i++){
+                    n = String.valueOf(i);
+                    mes.setCharAt(mes.indexOf("-"),'#');
+                    mes.replace(x,x+1,n);
+                    System.out.print("\033[H\033[2J");
+                    System.out.println(mes);
+
+                    try{
+                        Thread.sleep(rn.nextInt(1000));
+                    }catch (InterruptedException e){
+                        System.out.println("Interruption");
+                    }
+                }
+                System.out.print("\033[H\033[2J");
+                System.out.println("\nYou are in.\n");
+                zoo.sysExecution();
+            } else {
+                System.out.println("\nPassword is incorrect, try again.");
+            }
+        } while (!passIsValid);
+    }
+
+    }
